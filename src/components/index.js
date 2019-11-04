e55bdde13e57275cb43a45a80e61564a23b05ed1
JAAAAAEMKIM <jmkim326@gmail.com>
2019-11-04 19:43:36 +0900

    memo

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Header from './Header';
 import Authentication from './Authentication';
+import Write from './Write';
+import Memo from './Memo';
+import MemoList from './MemoList';
 
 
-export { Header, Authentication };
+export { Header, Authentication, Write, Memo, MemoList };
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,75 @@
 // src/containers/Home.js
 import React from 'react';
+import { connect } from 'react-redux';
+import { Write, MemoList } from 'components';
 
 class Home extends React.Component {
+    
+    constructor(props) {
+        super(props);        
+        this.handlePost = this.handlePost.bind(this);
+    }
+
+    /* POST MEMO */
+    handlePost(contents) {
+        return this.props.memoPostRequest(contents).then(
+            () => {
+                if(this.props.postStatus.status === "SUCCESS") {
+                    // TRIGGER LOAD NEW MEMO
+                    // TO BE IMPLEMENTED
+                    Materialize.toast('Success!', 2000);
+                } else {
+                    /*
+                        ERROR CODES
+                            1: NOT LOGGED IN
+                            2: EMPTY CONTENTS
+                    */
+                    let $toastContent;
+                    switch(this.props.postStatus.error) {
+                        case 1:
+                            // IF NOT LOGGED IN, NOTIFY AND REFRESH AFTER
+                            $toastContent = $('<span style="color: #FFB4BA">You are not logged in</span>');
+                            Materialize.toast($toastContent, 2000);
+                            setTimeout(()=> {location.reload(false);}, 2000);
+                            break;
+                        case 2:
+                            $toastContent = $('<span style="color: #FFB4BA">Please write something</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                        default:
+                            $toastContent = $('<span style="color: #FFB4BA">Something Broke</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                    }
+                }
+            }
+        );
+    }
+
+
     render() {
+        const write =(<Write onPost={this.handlePost}/>);
         return (
-            <div>
-                Home
+            <div className="wrapper">
+                {this.props.isLoggedIn ? write : undefined }
+                <MemoList/>
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.authentication.status.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        memoPostRequest: (contents) => {
+            return dispatch(memoPostRequest(contents));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
